0ce2c9acfc8d5cc06059c4bb0bde0a22039e3ace
P V UMESH CHANDRA <108012133+UmeshChandra2001@users.noreply.github.com>
2026-09-21 12:28:54 +0530

    Enable Jenkins service to start on boot

--- jenkins.sh
+++ jenkins.sh
@@ -2,4 +2,5 @@ yum install java-21-amazon-corretto -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/rpm-stable/jenkins.repo
 yum install jenkins -y
 systemctl start jenkins
+systemctl enable jenkins
 systemctl status jenkins
